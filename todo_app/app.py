699e04d020dfc9aca5125d69496275ec1a0ce558
Tim Kurian <timkurian@github.com>
2021-10-16 20:08:05 +0100

    Adding the first iteration
     this covers the basic create and retrieve operations

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
+from flask.templating import render_template
 from todo_app.flask_config import Config
+from todo_app.data import session_items
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +10,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todos = session_items.get_items()
+
+    return render_template('index.html', todos=todos)
+
+@app.route('/add_todo', methods=['POST'])
+def add_todo():
+    session_items.add_item(request.form['title'])
+    return index()
